8b45b5fbf82bb4c663403f10b43870946e640dc0
EnderFlop <enderflop@gmail.com>
2022-01-20 16:04:14 -0600

    Lab1
    Restructure and VSCode support
    Lab1

---
+++ Lab1/Problem1.java
@@ -0,0 +1,21 @@
+public class Problem1 {
+
+    // fix the errors in this program
+    // It should print
+    // Hello Class!
+    // 14
+    // This is 99
+    //
+    // Write a comment describing what was wrong with the program and how you fixed it
+    public static void main(String[] args) {
+      String x = "Hello Class!";
+      System.out.println(x);
+      int y = 7 + 7;
+      System.out.println(y);
+      y = 98;
+      y += 1;
+      System.out.println("This is " + y);
+      // I changed x to a String, then changed the reassignment of x to the initialization of y.
+        // I also changed the print statement to add y and added semicolons where needed.
+    }
+}
